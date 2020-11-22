d3faac92579f5e16be0630bce2f57b43c5517231
Murat Denizli <muratdenizli@gmail.com>
2020-11-22 11:58:27 +0300

    prettier and postcss config files added

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: {
+    'postcss-nested': {},
+    'postcss-custom-media': {
+      importFrom: [
+        {
+          customMedia: { '--p': '(min-width: 540px)' }
+        },
+        {
+          customMedia: { '--t': '(min-width: 980px)' }
+        },
+        {
+          customMedia: { '--d': '(min-width: 1270px)' }
+        }
+      ]
+    }
+  }
+}
