d36fce563cd2c7f93da4dadd85feae6cf4c9586a
Murat Denizli <muratdenizli@gmail.com>
2020-11-22 13:41:00 +0300

    navigation component added

---
+++ components/navigation/button.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import cn from 'classnames'
+
+import Button from '../button'
+
+import styles from './button.module.css'
+
+function NavigationButton({ notify, selected, children, className, ...props }) {
+  return (
+    <Button
+      className={cn(
+        styles.navButton,
+        selected && styles.navButtonSelected,
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Button>
+  )
+}
+
+export default NavigationButton
